fd32e2ed084b94aa17a07e5d06d3c20affb34381
Jussi Pakkanen <jpakkane@gmail.com>
2021-07-29 20:57:09 +0300

    Now with six first lines matching.

--- test_output.py
+++ test_output.py
@@ -6,17 +6,28 @@ original = 'original.asm'
 output = 'output.asm'
 
 pc = subprocess.Popen(['./aes_x86_64.py'], stdout=subprocess.PIPE)
+(stdo, _) = pc.communicate()
 
 if pc.returncode != 0:
     sys.exit(pc.returncode)
 
-open(output, 'wb').write(pc.stdout)
+open(output, 'wb').write(stdo)
 
-original_lines = open(original).read_lines()
-output_lines = open(output).read_lines()
+original_lines = []
 
-if len(original_lines) != len(output_lines):
-    sys.exit('Output files have different amount of lines.')
+for line in open(original):
+    line = line.strip()
+    if line:
+        original_lines.append(line)
+
+output_lines = []
+for line in open(output):
+    line = line.strip()
+    if line:
+        output_lines.append(line)
+
+#if len(original_lines) != len(output_lines):
+#    sys.exit('Output files have different amount of lines.')
 
 for i in range(len(original_lines)):
     if original_lines[i] != output_lines[i]:
